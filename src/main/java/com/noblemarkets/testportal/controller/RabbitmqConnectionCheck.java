982cebbc0ca1378e9f4ae14ec8a466a88c6eed0e
Igor Konovalov <ikonovi@gmail.com>
2021-07-23 15:53:07 +0300

    class to test connection to RMQ server on localhost

---
+++ src/main/java/com/noblemarkets/testportal/controller/RabbitmqConnectionCheck.java
@@ -0,0 +1,35 @@
+package com.noblemarkets.testportal.controller;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitmqConnectionCheck {
+    private static final Logger log = Logger.getLogger(RabbitmqConnectionCheck.class);
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        //factory.setPort(5672); // Default
+        factory.setPort(8080);
+        factory.setUsername("usr");
+        factory.setPassword("pwd");
+        Connection conn = null;
+        try {
+            conn = factory.newConnection();
+            log.info("Connection was opened successfully.");
+        } catch (IOException | TimeoutException exception) {
+            log.info("Connection failed");
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (IOException exception) {
+                    // do nothing
+                }
+            }
+        }
+    }
+}
